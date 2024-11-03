48896469e6e9e60ee83c511bd4be0dd632ffa1f8
Nguyễn Quang Khải <124948877+nqkhai294@users.noreply.github.com>
2024-11-03 21:49:59 +0700

    feat(learningContent): use props, button next prev
    feat(Category): create 2 interface
    
    fix(css): button menu toggle

--- src/app/(Base)/course_base/learning/page.tsx
+++ src/app/(Base)/course_base/learning/page.tsx
@@ -1,8 +1,18 @@
+'use client'
+import { Word } from "@/components/courses_base/category/Category";
 import LearningContent from "@/components/courses_base/learning_content/LearningContent";
+import { useEffect } from "react";
 
 const LearningCoursePage = (props: any) => {
+    
+    let learningName = sessionStorage.getItem("learningName") || "";
+    
+    let learningData = JSON.parse(sessionStorage.getItem("learningData") || "[]");
+    console.log(">>>Checkdata:  ", learningData);
+    console.log(">>>Checkname:  ", learningName);
+  
     return (
-        <LearningContent />
+        <LearningContent data={learningData} name={learningName} />
     );
 }
 
