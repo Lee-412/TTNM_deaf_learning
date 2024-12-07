b8ed4bf7e98ada57e186298012d705874c5fdcbf
Nguyễn Quang Khải <124948877+nqkhai294@users.noreply.github.com>
2024-12-07 09:20:20 +0700

    feat(faqPage, aboutPage): add page

--- src/app/(Base)/about/page.tsx
+++ src/app/(Base)/about/page.tsx
@@ -3,7 +3,7 @@ import AboutBase from "@/components/about/about";
 const About = () => {
     return (
         <>
-            About page
+            
             <AboutBase />
         </>
     )
